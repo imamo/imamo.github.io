cda87112d0919ce6879f8bbf26fb4e86cb8fc6d2
IMAMO <info@imamo.si>
2013-06-28 16:29:23 +0200

    polna kapa obiska prispevek, vimeo embbedd rb in shortcode

---
+++ _plugins/vimeo.rb
@@ -0,0 +1,28 @@
+class Vimeo < Liquid::Tag
+  Syntax = /^\s*([^\s]+)(\s+(\d+)\s+(\d+)\s*)?/
+
+  def initialize(tagName, markup, tokens)
+    super
+
+    if markup =~ Syntax then
+      @id = $1
+
+      if $2.nil? then
+          @width = 560
+          @height = 420
+      else
+          @width = $2.to_i
+          @height = $3.to_i
+      end
+    else
+      raise "No VImeo ID provided in the \"vimeo\" tag"
+    end
+  end
+
+  def render(context)
+    # "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}\" frameborder=\"0\"allowfullscreen></iframe>"
+    "<iframe src=\"http://player.vimeo.com/video/#{@id}\" frameborder=\"0\" width=\"700\" height=\"394\">        </iframe>"
+  end
+
+  Liquid::Template.register_tag "vimeo", self
+end
---
+++ _posts/2013-06-28-polna-mera-obiska.md
@@ -0,0 +1,16 @@
+---
+layout: post
+title: "Polna mera obiska spletne strani"
+description: "Tako izgleda ko se zgodi da imaš zvrhano mero obiska na spletni strani."
+category: splet 
+tags: [obisk, zahtevki, spletna stran, spletno gostovanje]
+---
+{% include JB/setup %}
+
+Ste že kdaj videli kako izgleda ko imaš na sekundo nekaj sto ali tisoč zahtevkov za ogled spletne strani? No tukaj je en lep prikaz ;)
+
+{% vimeo 69317575 %}
+
+*epp: seveda za [strežnik na katerem spletne stran gostuje][1] ni težav z obdelavo vseh zahtevkov ;)*
+
+[1]:http://www.spletnaabeceda.si
